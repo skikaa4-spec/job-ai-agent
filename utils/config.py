54c151faf9d87804469993887d65fede2153d979
skikaa4-spec <skikaa4@gmail.com>
2026-07-07 14:55:54 +0100

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,26 @@
+EMAIL = "skikaa4@gmail.com"
+ 
+KEYWORDS = [
+    "QHSE",
+    "Safety",
+    "HSE",
+    "Warehouse",
+    "Logistics",
+    "Stock",
+    "Maintenance",
+    "Supervisor",
+    "Technician",
+    "Odoo"
+]
+ 
+COUNTRIES = [
+    "Morocco",
+    "France",
+    "Belgium",
+    "Germany",
+    "Canada",
+    "United Kingdom",
+    "United Arab Emirates",
+    "Qatar",
+    "Saudi Arabia"
+]
