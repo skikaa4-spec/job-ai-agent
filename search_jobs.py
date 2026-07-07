85db23ed952eb3bc2e3dc0bf083b42e8c7fe1099
skikaa4-spec <skikaa4@gmail.com>
2026-07-07 11:18:03 +0100

    Update search_jobs.py
    def search_jobs():
        jobs = [
            {
                "title": "Technicien Informatique",
                "company": "Entreprise Maroc",
                "location": "Casablanca",
                "link": "https://emploi.ma"
            },
            {
                "title": "Assistant Logistique",
                "company": "France Travail",
                "location": "France",
                "link": "https://francetravail.fr"
            }
        ]
     
        return jobs

--- search_jobs.py
+++ search_jobs.py
@@ -1,1 +1,17 @@
-
+def search_jobs():
+    jobs = [
+        {
+            "title": "Technicien Informatique",
+            "company": "Entreprise Maroc",
+            "location": "Casablanca",
+            "link": "https://emploi.ma"
+        },
+        {
+            "title": "Assistant Logistique",
+            "company": "France Travail",
+            "location": "France",
+            "link": "https://francetravail.fr"
+        }
+    ]
+ 
+    return jobs
