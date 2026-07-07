68ed6bce9a877c15a2b479429037922f80917637
skikaa4-spec <skikaa4@gmail.com>
2026-07-07 11:17:13 +0100

    Update search_jobs.py

--- search_jobs.py
+++ search_jobs.py
@@ -1,15 +1,1 @@
-def search_jobs():
-    jobs = [
-        {
-            "title": "Magasinier",
-            "company": "Entreprise A",
-            "city": "Casablanca"
-        },
-        {
-            "title": "Gestionnaire de Stock",
-            "company": "Entreprise B",
-            "city": "Rabat"
-        }
-    ]
- 
-    return jobs
+
