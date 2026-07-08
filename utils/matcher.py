3a6370e9bfe839b11841007031880fc6bcc2d0cc
skikaa4-spec <skikaa4@gmail.com>
2026-07-08 09:20:37 +0100

    Update matcher.py

--- utils/matcher.py
+++ utils/matcher.py
@@ -1,10 +1,12 @@
 def calculate_match(cv_text, keywords):
-    cv = cv_text.lower()
+    if not keywords:
+        return 0
  
+    cv = cv_text.lower()
     score = 0
  
     for keyword in keywords:
         if keyword.lower() in cv:
             score += 1
  
-    return round(score / len(keywords) * 100)
+    return round((score / len(keywords)) * 100)
