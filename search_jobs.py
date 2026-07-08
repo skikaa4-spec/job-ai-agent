d1f90fd9e10f239cb0b9ac2017d7df8751d00d44
skikaa4-spec <skikaa4@gmail.com>
2026-07-08 10:29:24 +0100

    Update search_jobs.py

--- search_jobs.py
+++ search_jobs.py
@@ -1,8 +1,11 @@
 from search.remotive import search_remotive
+from search.emploi_ma import search_emploi_ma
  
  
 def search_jobs():
     jobs = []
+ 
+    jobs.extend(search_emploi_ma())
  
     jobs.extend(search_remotive())
  
