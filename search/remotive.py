c1ba57fa8c7811f67f93bfea9355ced0fe3a988f
skikaa4-spec <skikaa4@gmail.com>
2026-07-08 09:26:09 +0100

    Update remotive.py

--- search/remotive.py
+++ search/remotive.py
@@ -1,5 +1,18 @@
 import requests
  
+KEYWORDS = [
+    "QHSE",
+    "Safety",
+    "HSE",
+    "Warehouse",
+    "Logistics",
+    "Stock",
+    "Maintenance",
+    "Supervisor",
+    "Technician",
+    "Odoo"
+]
+ 
 def search_remotive():
     url = "https://remotive.com/api/remote-jobs"
  
@@ -9,18 +22,22 @@ def search_remotive():
  
         jobs = []
  
-        for job in data["jobs"][:20]:
-            jobs.append({
-                "title": job["title"],
-                "company": job["company_name"],
-                "location": job["candidate_required_location"],
-                "link": job["url"],
-                "keywords": []
-            })
+        for job in data["jobs"]:
+ 
+            title = job["title"].lower()
+ 
+            if any(word.lower() in title for word in KEYWORDS):
+ 
+                jobs.append({
+                    "title": job["title"],
+                    "company": job["company_name"],
+                    "location": job["candidate_required_location"],
+                    "link": job["url"],
+                    "keywords": KEYWORDS
+                })
  
         return jobs
  
     except Exception as e:
         print("Erreur:", e)
         return []
- 
