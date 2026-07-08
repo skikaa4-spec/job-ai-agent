4e202d2272509fdfd2019167c0d460e6423727b5
skikaa4-spec <skikaa4@gmail.com>
2026-07-08 10:21:16 +0100

    Update matcher.py

--- utils/matcher.py
+++ utils/matcher.py
@@ -1,12 +1,25 @@
-def calculate_match(cv_text, keywords):
-    if not keywords:
+import re
+ 
+ 
+def extract_keywords(text):
+    words = re.findall(r"[a-zA-ZÀ-ÿ]{3,}", text.lower())
+ 
+    stop_words = {
+        "the", "and", "for", "with", "from", "dans", "pour",
+        "les", "des", "une", "est", "sur", "par", "aux",
+        "de", "du", "la", "le", "et", "ou", "en"
+    }
+ 
+    return set(word for word in words if word not in stop_words)
+ 
+ 
+def calculate_match(cv_text, title, description):
+    cv_keywords = extract_keywords(cv_text)
+    job_keywords = extract_keywords(title + " " + description)
+ 
+    if not job_keywords:
         return 0
  
-    cv = cv_text.lower()
-    score = 0
+    common = cv_keywords.intersection(job_keywords)
  
-    for keyword in keywords:
-        if keyword.lower() in cv:
-            score += 1
- 
-    return round((score / len(keywords)) * 100)
+    return round((len(common) / len(job_keywords)) * 100)
