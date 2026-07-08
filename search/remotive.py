89c97c19801f2cb5fadef882dd20deca3d113781
skikaa4-spec <skikaa4@gmail.com>
2026-07-08 09:00:02 +0100

    Update remotive.py

--- search/remotive.py
+++ search/remotive.py
@@ -1,1 +1,24 @@
-
+import requests
+ 
+def search_remotive():
+    url = "https://remotive.com/api/remote-jobs"
+ 
+    try:
+        response = requests.get(url, timeout=10)
+        data = response.json()
+ 
+        jobs = []
+ 
+        for job in data["jobs"][:20]:
+            jobs.append({
+                "title": job["title"],
+                "company": job["company_name"],
+                "location": job["candidate_required_location"],
+                "link": job["url"]
+            })
+ 
+        return jobs
+ 
+    except Exception as e:
+        print("Erreur:", e)
+        return []
